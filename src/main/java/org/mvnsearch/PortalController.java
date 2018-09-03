cc9cdb6d996642632ae8675300ac03195ff2ccb3
a.pomosov <a.pomosov@alibaba-inc.com>
2018-09-03 19:01:58 +0300

    More syntactic features used in templates for better understanding

--- src/main/java/org/mvnsearch/PortalController.java
+++ src/main/java/org/mvnsearch/PortalController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+
 /**
  * portal controller
  *
@@ -16,7 +18,7 @@ public class PortalController {
     @GetMapping(value = "/")
     @ResponseBody
     public RockerModel index() {
-        return templates.Index.template("Welcome", "Hi, Welcome to Rocker!");
+        return templates.Main.template("Welcome", "Sasha", Arrays.asList("Sasha", "Jackie"), true);
     }
 
     @GetMapping("/hello")
